e79758e8589702f454ad26db4d9eaa5f9d32bb62
user <user@test.ru>
2023-12-30 15:20:52 +0500

    Добавьте файлы проекта.

---
+++ Monitor/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace Monitor
+{
+    public partial class Form1 : Form
+    {
+
+        [DllImport("user32.dll")]
+        private static extern int SendMessage(int hWnd, int hMsg, int wParam, int lParam);
+
+        private void SetMonitorState(MonitorState state)
+        {
+            SendMessage(0xFFFF, 0x112, 0xF170, (int)state);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Opacity = 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SetMonitorState(MonitorState.Off);
+
+            Thread.Sleep(5000);
+
+            SetMonitorState(MonitorState.On);
+
+            Application.Exit();
+        }
+    }
+}
